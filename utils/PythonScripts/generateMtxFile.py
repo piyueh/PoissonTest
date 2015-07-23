07551252e8a52ec7a31f3faa450c3f428c706cd5
Pi-Yueh Chuang <pychuang@gwu.edu>
2015-07-23 12:52:43 -0400

    modify the generateMtxFile.py to remove the comments before rhs and solution vectors, and also add an AmgX command at the beginning of generated mtx file

--- utils/PythonScripts/generateMtxFile.py
+++ utils/PythonScripts/generateMtxFile.py
@@ -7,7 +7,7 @@ from generateVecs import p_extSoln, RHS
 
 
 L = 1.0
-Nx = Ny = 100
+Nx = Ny = 1000
 dL = L / Nx
 
 n = 1.0
@@ -22,14 +22,11 @@ A = generateA(Nx, Ny)
 p = numpy.zeros(Nx * Ny)
 f = RHS(X, Y, n, dL)
 
-io.mmwrite("AmgXsystem.mtx", A)
+io.mmwrite("AmgXsystem.mtx", A, "%AMGX rhs solution")
 
 file = open("AmgXsystem.mtx", "ab")
 
-file.write(bytes("%%rhs\n", "UTF-8"))
 numpy.savetxt(file, f, '%.18e')
-
-file.write(bytes("%%solution\n", "UTF-8"))
 numpy.savetxt(file, p, '%.18e')
 
 file.close()
